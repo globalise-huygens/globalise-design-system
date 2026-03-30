7db50721bb8d274e9632b25bf4334031aab95216
Jona Schlegel <jona.schlegel@gmail.com>
2026-03-30 10:05:03 +0200

    restore design landing page as root

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,16 +1,1 @@
-import { DocsSidebar } from "@/components/DocsSidebar";
-import { Container } from "@globalise/design-system";
-import GettingStarted from "./docs/getting-started/page.mdx";
-
-export default function RootPage() {
-  return (
-    <Container className="py-12 lg:py-16">
-      <div className="flex gap-12">
-        <DocsSidebar />
-        <article className="min-w-0 flex-1 max-w-3xl">
-          <GettingStarted />
-        </article>
-      </div>
-    </Container>
-  );
-}
+export { default } from "./example/page";
